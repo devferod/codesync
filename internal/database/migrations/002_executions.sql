57127e2ace9bb761807060ff3941e1b19bfbe293
fernando <dev.ferod@gmail.com>
2026-02-26 20:42:19 -0300

    add exec table

---
+++ internal/database/migrations/002_executions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE executions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    repository_id UUID NOT NULL,
+    target_id UUID NOT NULL,
+    status TEXT NOT NULL,
+    started_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    finished_at TIMESTAMP,
+    FOREIGN KEY (repository_id) REFERENCES repositories(id),
+    FOREIGN KEY (target_id) REFERENCES replication_targets(id)
+);
